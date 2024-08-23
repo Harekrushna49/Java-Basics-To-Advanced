c2a9b7f8ec56e3c542a7294e75e0794d50ee6b02
harekrushna49 <harekrushnanayak749@gmail.com>
2024-08-23 19:56:34 +0530

    Return Type

---
+++ Void_return_type.java
@@ -0,0 +1,8 @@
+public class Void_return_type {
+        static void type(){
+            System.out.println("Hello");
+        }
+        public static void main(String[] args) {
+            type();
+        }
+}
