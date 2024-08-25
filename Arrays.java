40124dfe5da9df248e7f6172e388cacc119c1f22
harekrushna49 <harekrushnanayak749@gmail.com>
2024-08-25 14:56:25 +0530

    Array Program

---
+++ Arrays.java
@@ -0,0 +1,18 @@
+public class Arrays {
+        public static void main(String[] args) {
+
+            //1. 1-dimensional Array
+            int a [] = new int[5];
+            a[0] = 234;
+            a[1] = 865;
+            a[2] = 310;
+            a[3] = 468;
+            a[4] = 975;
+            System.out.println(a[4]);
+
+            // another type of initialisation.
+            int l [] = {1478, 865, 7893, 4874, 8224};
+            System.out.println(l[3]);
+            System.out.println(l.length);
+        }
+}
